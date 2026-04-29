84ea0d3a2fec5d94ff5391405ec9de14d735cff6
MaxUilanov09 <max.ulyanov7@gmail.com>
2026-04-29 17:45:15 +0100

    main start

--- src/js/api.js
+++ src/js/api.js
@@ -2,9 +2,22 @@ const MAIN_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?'
 const API_KEY = 'apikey=5P2eENX3jSAJ1avlQtGveA6HpNKohevi'
 
 
-async function getEvents(queryOptions = []) {
+export function getEventData(data) {
+    return data._embedded.events;
+}
+
+export async function getEvents(queryOptions) {
     try {
         let path = '';
+        if (queryOptions.code !== -1) {
+            path += `&countryCode=${queryOptions.code}`;
+        }
+        if (queryOptions.query !== '') {
+            path += `&keyword=${queryOptions.query}`;
+        }
+        if (queryOptions.page !== 0) {
+            path += `&page=${queryOptions.page}`;
+        }
         const res = await fetch(MAIN_URL + API_KEY + path);
         const data = await res.json();
         return data;
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
-
-
-// console.log(getEvents());
-
 import {} from './logo.js';
-import {} from './api.js';
+import { getEvents, getEventData } from './api.js';
+import { fillDropdown } from './dropdown.js';
+import { fillCardList } from './card.js';
+import {} from './modal/modal.js';
+
+const dropdownDiv = document.querySelector('.dropdown__content');
+const eventCardList = document.querySelector('.event__card__list');
+
+
+let PathOptions = {
+    code: -1, 
+    query: '', 
+    page: 0
+};
+
+fillDropdown(dropdownDiv, PathOptions);
+
+getEvents(PathOptions)
+    .then(data => fillCardList(eventCardList, getEventData(data)))
