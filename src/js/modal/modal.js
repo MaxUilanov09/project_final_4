159909b0931fc5e9effdef0e189ad98e300b5faf
Yegor-77 <egorhmeluk77@gmail.com>
2026-05-06 20:03:45 +0200

    06,05,2026

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,41 +1,37 @@
-import { getEvents, getEventData } from '../api.js';
+import { getEvents, getEventData } from "../api.js";
 
 const modal = document.getElementById("modal");
 
 const modalImage = document.getElementById("modal-image");
 const modalTitle = document.getElementById("modal-title");
-
 const modalDate = document.getElementById("modal-date");
 const modalDescription = document.getElementById("modal-description");
-
 const modalLink = document.getElementById("modal-link");
-
-const closeBtn = document.querySelector(".modal-close");
-
-
 const modalWho = document.getElementById("modal-who");
 const modalWhere = document.getElementById("modal-where");
 
-function fillModal(data) { // замінити:      card.dataset.  ---->.  data.
-    // і поміняти image,title,description,date... на правильні
-    // щоб дізнатися правильні запусти і подивися на обʼєкт у консолі
-  modalImage.src = card.dataset.image;
-  modalTitle.textContent = card.dataset.title;
+const closeBtn = document.querySelector(".modal-close");
 
-  
-  modalDescription.textContent = card.dataset.description;
+function fillModal(data) {
+  const event = getEventData(data)[0];
 
-  
-  modalDate.textContent = card.dataset.date;
+  modalImage.src = event.images?.[0]?.url || "";
+  modalTitle.textContent = event.name || "";
 
-  
-  modalWho.textContent = card.dataset.title;
+  modalDescription.textContent = event.info || "";
+  modalDate.textContent =
+    (event.dates?.start?.localDate || "") +
+    " " +
+    (event.dates?.start?.localTime || "");
 
-  
-  modalWhere.textContent = card.dataset.where || "Unknown location";
+  modalWho.textContent = event._embedded?.attractions?.[0]?.name || "";
 
-  
-  modalLink.href = card.dataset.link;
+  modalWhere.textContent =
+    (event._embedded?.venues?.[0]?.city?.name || "") +
+    ", " +
+    (event._embedded?.venues?.[0]?.country?.name || "");
+
+  modalLink.href = event.url || "#";
 
   modal.style.display = "flex";
 }
@@ -44,16 +40,12 @@ document.querySelector(".event__card__list").addEventListener("click", (e) => {
   const card = e.target.closest(".event__card");
   if (!card) return;
 
-  getEvents({id: card.dataset.id})
-    .then(data => {console.log(getEventData(data)[0]); return data;})
-    .then(data => fillModal(data));
+  getEvents({ id: card.dataset.id }).then((data) => fillModal(data));
 });
-
 
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
 
 modal.addEventListener("click", (e) => {
   if (e.target === modal) {
